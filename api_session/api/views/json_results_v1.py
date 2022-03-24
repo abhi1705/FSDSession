ff0db168be10a908cc1e28157610e3071a835af6
Aditya Kothari <aditya.kothari@zs.com>
2022-03-24 15:17:23 +0530

    adding /train to api

--- api_session/api/views/json_results_v1.py
+++ api_session/api/views/json_results_v1.py
@@ -8,3 +8,11 @@ api_blueprint = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 def get_results_api_v1(id):
     result = g.session.get(Results, id)
     return make_response(jsonify(result.as_dict())), 200
+
+@api_blueprint.route('/train', methods=['POST'])
+def train_model():
+    if not request.json or not 'method' in request.json:
+        return make_response(jsonify("'method' not specified")), 400
+    else:
+        method = request.json['method']
+    return make_response(jsonify(f"Retraining Model using '{method}'...")), 200
