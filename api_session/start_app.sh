1985f4aed2b1ecb00bfe639ffb12494f04aabaf2
Aditya Kothari <aditya.kothari@zs.com>
2022-03-24 15:49:32 +0530

    adding startp

--- api_session/start_app.sh
+++ api_session/start_app.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/logs
-if nohup `which python3` run.py > ~/logs/log.txt 2>&1 & ; then
+nohup `which python3` run.py > ~/logs/log.txt 2>&1 &
+if [ $? -eq 0 ] ; then
     echo "Started App..."
 else
     echo "Failed to start App"
